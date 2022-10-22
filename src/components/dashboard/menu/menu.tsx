53a65ba18a5bbf63ec855c5505ac33863538690a
HerdiEhzaSaputra <herdi.ehza@icloud.com>
2022-10-22 19:12:02 +0700

    fix if statment to assign class

--- src/components/dashboard/menu/menu.tsx
+++ src/components/dashboard/menu/menu.tsx
@@ -7,7 +7,6 @@ export default component$(() => {
     const loc = useLocation();
     const { pathname } = useLocation();
 
-
     return (
         <>
             {
@@ -20,9 +19,10 @@ export default component$(() => {
                                         <Link
                                             href={item.href}
                                             aria-label={item.href}                                                                                                                                                            
-                                            class={{
-                                                'relative px-4 py-3 flex items-center space-x-4 rounded-xl text-white bg-gradient-to-r from-sky-600 to-cyan-400': loc.pathname === item.href, 'px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group': !pathname.startsWith(item.href),
-                                            }}
+                                            class={(loc.pathname === item.href ? 'relative px-4 py-3 flex items-center space-x-4 rounded-xl text-white bg-gradient-to-r from-sky-600 to-cyan-400' : 'px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group')}
+                                            // class={{
+                                            //     'relative px-4 py-3 flex items-center space-x-4 rounded-xl text-white bg-gradient-to-r from-sky-600 to-cyan-400': loc.pathname === item.href, 'px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group': !pathname.startsWith(item.href),
+                                            // }}
                                         >
                                             <svg class="-ml-1 h-6 w-6" viewBox="0 0 24 24" fill="none">
                                                 <path d="M6 8a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2V8ZM6 15a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2v-1Z" class="fill-current text-cyan-400 dark:fill-slate-600"></path>
@@ -30,9 +30,7 @@ export default component$(() => {
                                                 <path d="M13 15a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2h-1a2 2 0 0 1-2-2v-1Z" class="fill-current group-hover:text-sky-300"></path>
                                             </svg>
                                             <span
-                                                class={{
-                                                    '-mr-1 font-medium': loc.pathname === item.href, 'group-hover:text-gray-700': !pathname.startsWith(item.href),
-                                                }}
+                                                class={(loc.pathname === item.href ? '-mr-1 font-medium' : 'group-hover:text-gray-700')}
                                             >{item.text}</span>
                                         </Link>
                                     </li>
